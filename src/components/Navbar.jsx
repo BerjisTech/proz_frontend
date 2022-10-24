7c6b4abf6679ef1fda5adb6d5ae33eb8e9e8ce94
prometheus <berjistechnologies@gmail.com>
2022-10-25 00:25:16 +0300

    General dashboard navigation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,10 +3,18 @@ import { FiSettings, FiHome, FiMessageCircle, FiThumbsUp, FiBookOpen, FiShopping
 import { Link } from 'react-router-dom'
 import Spacer from './Spacer'
 
+// let primary_nav_dom = document.querySelector('.primaryDashboardNavigation')
+// let secondary_nav_dom = document.querySelector('.secondaryDashboardNavigation')
+// // secondary_nav_dom.classList.toggle('hidden')
+// primary_nav_dom.addEventListener('click', (e) => {
+//     e.preventDefault()
+//     secondary_nav_dom.classList.toggle('hidden')
+// })
+
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg min-vh-100 bg-primary m-0 p-0">
-            <div className="container-fluid m-0 p-0 h-screen d-flex flex-column flex-nowrap sticky overflow-auto">
+        <nav className="navbar navbar-expand-lg min-vh-100 bg-primary m-0 p-0 d-flex">
+            <div className="m-0 p-0 h-screen d-flex flex-column flex-nowrap sticky overflow-auto">
                 <header className="w-full px-3 pt-3 border-bottom sticky bg-primary shadow">
                     <a className="navbar-brand m-0 p-0" href="/">
                         <div className="d-flex align-items-center justify-content-center">
@@ -21,47 +29,80 @@ const Navbar = () => {
                     </button>
                 </header>
                 <div className="collapse navbar-collapse d-flex flex-column align-items-center justify-content-between" id="sideBarNav">
-                    <ul className="navbar-nav d-flex flex-column w-full align-items-start justify-content-start">
-                        <li className="nav-item w-full py-2">
+                    <ul className="navbar-nav d-flex flex-column w-full align-items-center justify-content-center primaryDashboardNavigation">
+                        <li className="nav-item w-full text-center py-2">
                             <Link to="/dashboard" role="tooltip" data-bs-tooltip="Home" className="nav-link d-flex align-items-center overflow-hidden text-white">
                                 <FiHome className="app__sidebar-icon" />
                             </Link>
                         </li>
-                        <li className="nav-item w-full py-2">
+                        <li className="nav-item w-full text-center py-2">
                             <Link to="/dashboard/findwork" className="nav-link d-flex align-items-center overflow-hidden text-white">
                                 <FiPieChart className="app__sidebar-icon" />
                             </Link>
                         </li>
-                        <li className="nav-item w-full py-2">
+                        <li className="nav-item w-full text-center py-2">
                             <Link to="/dashboard/applications" className="nav-link d-flex align-items-center overflow-hidden text-white">
                                 <FiShoppingCart className="app__sidebar-icon" />
                             </Link>
                         </li>
-                        <li className="nav-item w-full py-2">
+                        <li className="nav-item w-full text-center py-2">
                             <Link to="/dashboard/courses" className="nav-link d-flex align-items-center overflow-hidden text-white">
                                 <FiBookOpen className="app__sidebar-icon" />
                             </Link>
                         </li>
-                        <li className="nav-item w-full py-2">
+                        <li className="nav-item w-full text-center py-2">
                             <Link to="/dashboard/kudoz" className="nav-link d-flex align-items-center overflow-hidden text-white">
                                 <FiThumbsUp className="app__sidebar-icon" />
                             </Link>
                         </li>
-                        <li className="nav-item w-full py-2">
+                        <li className="nav-item w-full text-center py-2">
                             <Link to="/dashboard/messages" className="nav-link d-flex align-items-center overflow-hidden text-white">
                                 <FiMessageCircle className="app__sidebar-icon" />
                             </Link>
                         </li>
                     </ul>
                     <ul className="navbar-nav d-flex flex-column w-full align-items-start justify-content-start">
-                        <li className="nav-item w-full py-2">
+                        <li className="nav-item w-full text-center py-2">
                             <Link to="/dashboard/settings" className="nav-link d-flex align-items-center overflow-hidden text-white">
                                 <FiSettings className="app__sidebar-icon" />
                             </Link>
                         </li>
                     </ul>
                 </div>
-
+            </div>
+            <div className="m-0 p-0 h-screen w-[200px] bg-green-50 secondaryDashboardNavigation">
+                <ul className="navbar-nav d-flex flex-column w-full align-items-center justify-content-center primaryDashboardNavigation">
+                    <li className="nav-item w-full text-center py-2">
+                        <Link to="/dashboard" role="tooltip" data-bs-tooltip="Home" className="nav-link d-flex align-items-center overflow-hidden text-[#186362]">
+                            Home
+                        </Link>
+                    </li>
+                    <li className="nav-item w-full text-center py-2">
+                        <Link to="/dashboard/findwork" className="nav-link d-flex align-items-center overflow-hidden text-[#186362]">
+                            Find Work
+                        </Link>
+                    </li>
+                    <li className="nav-item w-full text-center py-2">
+                        <Link to="/dashboard/applications" className="nav-link d-flex align-items-center overflow-hidden text-[#186362]">
+                            Applications
+                        </Link>
+                    </li>
+                    <li className="nav-item w-full text-center py-2">
+                        <Link to="/dashboard/courses" className="nav-link d-flex align-items-center overflow-hidden text-[#186362]">
+                            Courses
+                        </Link>
+                    </li>
+                    <li className="nav-item w-full text-center py-2">
+                        <Link to="/dashboard/kudoz" className="nav-link d-flex align-items-center overflow-hidden text-[#186362]">
+                            Kudoz
+                        </Link>
+                    </li>
+                    <li className="nav-item w-full text-center py-2">
+                        <Link to="/dashboard/messages" className="nav-link d-flex align-items-center overflow-hidden text-[#186362]">
+                            Messages
+                        </Link>
+                    </li>
+                </ul>
             </div>
         </nav>
     )
