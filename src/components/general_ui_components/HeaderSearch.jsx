38dd674e92824d2b51462d369f0648390f7d86e7
prometheus <berjistechnologies@gmail.com>
2022-10-25 20:50:36 +0300

    Custom placeholder

--- src/components/general_ui_components/HeaderSearch.jsx
+++ src/components/general_ui_components/HeaderSearch.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import 'react-icons/fi'
 import { FiSearch } from 'react-icons/fi'
 
-const HeaderSearch = () => {
+const HeaderSearch = ({ placeholder }) => {
     return (
         <div className="flex-fill">
             <form className="d-flex flex-row align-items-center justify-content-center ms-5" action='/dashboard/search' method='GET'>
                 <FiSearch className="fs-3 me-3 text-gray-400" />
-                <input type="text" placeholder="Search..." name="searchItem" className='flex-fill border-0 bg-gray-100 rounded-5 bg-gray py-2 px-5' />
+                <input type="text" placeholder={placeholder ? placeholder : "Search..."} name="searchItem" className='flex-fill border-0 bg-gray-100 rounded-5 bg-gray py-2 px-5' />
             </form>
         </div>
     )
--- src/components/navigation/UserFacingTopNavigation.jsx
+++ src/components/navigation/UserFacingTopNavigation.jsx
@@ -9,19 +9,23 @@ const UserFacingTopNavigation = () => {
         <nav className="navbar navbar-expand-lg bg-white navbar-light sticky w-full">
             <div className="d-flex align-items-center justify-content-center w-full py-2 shadow">
                 <img src='/images/logos/logo.png' alt="logo" className="w-20 ms-3" />
-                <HeaderSearch />
+                <HeaderSearch placeholder="What are you looking for?"/>
                 <ul className="navbar-nav mb-2 mb-lg-0 flex-fill justify-content-end me-3">
                     <li className="nav-item dropdown">
                         <a className="nav-link dropdown-toggle d-flex align-items-center justify-content-center" href="/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                             <BiGlasses />
-                            <span className="ms-2">Start Translating</span>
+                            <span className="ms-2">Terminology</span>
                         </a>
                         <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li className='p-2 fw-bold'>Kudoz Help Netwrok</li>
                             <li><a className="dropdown-item" href="/hire/freelancers">Hire Freelancers</a></li>
                             <li><a className="dropdown-item" href="/hire/agencies">Hire Agencies</a></li>
                             <li><hr className="dropdown-divider" /></li>
+                            <li className='p-2 fw-bold'>Other</li>
                             <li><a className="dropdown-item" href="/jobs/translate">Translating Jobs</a></li>
                             <li><a className="dropdown-item" href="/jobs/interprete">Interpretation Jobs</a></li>
+                            <li><hr className="dropdown-divider" /></li>
+                            <li className='p-2 fw-bold'>Learn More About</li>
                         </ul>
                     </li>
 
