1380b5ddbc1ad650ab65d7fa89d71a4af1a81f73
prometheus <berjistechnologies@gmail.com>
2022-11-05 01:12:04 +0300

    Remove console.log(event)

--- src/pages/events/Events.jsx
+++ src/pages/events/Events.jsx
@@ -51,7 +51,6 @@ const Events = () => {
                     {events.map((event) => {
                         const { id, name, description, url, start, end, logo, organization, venue, format, online_event } = event
                         return (
-                            console.log(event),
                             <div className="w-full d-flex gap-3 flex-wrap algin-items-center justify-content-between hover:shadow-xl px-2 py-4" key={id}>
                                 <div className='min-w-70px'>
                                     <img src={logo.url} className="img-fluid rounded-start w-[100px]" alt={name.text} />
