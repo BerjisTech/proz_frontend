060936a5fdbee2bf2cece1f43ae45ada6e0dc0dd
prometheus <berjistechnologies@gmail.com>
2022-11-08 21:23:40 +0300

    Fix dashboard routes

--- src/components/Notifications.jsx
+++ src/components/Notifications.jsx
@@ -6,7 +6,7 @@ const Notifications = () => {
         <div>
             <a className="text-green-800 bg-inherit mx-2 d-flex align-items-center justify-content-center" id="notificationsDropdown" href="/dashboard/*" data-bs-toggle="dropdown" aria-expanded="false">
                 <FiBell className="text-xl text-orange-400" />
-                <span class="position-relative top-0 start-0 p-1 translate-middle badge rounded-pill bg-danger">+99 <span class="visually-hidden">unread messages</span></span>
+                <span className="position-relative top-0 start-0 p-1 translate-middle badge rounded-pill bg-danger">+99 <span className="visually-hidden">unread messages</span></span>
             </a>
             <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="notificationsDropdown">
                 <li className='px-2 text-[12px] text-green-800'><a className="dropdown-item" href="/dashboard/notifications/1">New Job received</a></li>
